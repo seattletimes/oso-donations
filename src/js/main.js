6c20da16eef63435aa4ca5239c370ca0841f6c21
Audrey Carlsen <acarlsen@seattletimes.com>
2015-03-17 16:04:35 -0700

    get rid of complicated collide function

--- src/js/main.js
+++ src/js/main.js
@@ -166,33 +166,6 @@ force.on("tick", function () {
       .attr("cy", function (d) { return d.y; });
   d3.selectAll("text").attr("x", function (d) { return d.x; })
       .attr("y", function (d) { return d.y; });
-  node.each(collide(0.5));
+  // node.each(collide(0.5));
 });
 
-var padding = 25, // separation between circles
-    radius = 1;
-function collide(alpha) {
-  var quadtree = d3.geom.quadtree(nodes);
-  return function(d) {
-    var rb = 2 * radius + padding,
-        nx1 = d.x - rb,
-        nx2 = d.x + rb,
-        ny1 = d.y - rb,
-        ny2 = d.y + rb;
-    quadtree.visit(function(quad, x1, y1, x2, y2) {
-      if (quad.point && (quad.point !== d)) {
-        var x = d.x - quad.point.x,
-            y = d.y - quad.point.y,
-            l = Math.sqrt(x * x + y * y);
-          if (l < rb) {
-          l = (l - rb) / l * alpha;
-          d.x -= x *= l;
-          d.y -= y *= l;
-          quad.point.x += x;
-          quad.point.y += y;
-        }
-      }
-      return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
-    });
-  };
-}
